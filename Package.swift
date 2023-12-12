117291536b74d5fc76fbd3d8c5a9e0b5d76189b2
Alexander Balaban <balaban.alexander+github@gmail.com>
2023-12-12 01:41:20 +0000

    eh, depl target

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ import CompilerPluginSupport
 let package = Package(
     name: "aoc-utils",
     platforms: [
-       .macOS(.v10_15),
+       .macOS(.v10_13),
     ],
     products: [
         .library(
